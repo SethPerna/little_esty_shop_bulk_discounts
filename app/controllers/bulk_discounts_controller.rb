33a54258e1b3b10862304deea12f04fd7def5d63
Seth Perna <gseth26@gmail.com>
2022-01-16 14:06:20 -0500

    added edit functionality

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,26 +1,49 @@
 class BulkDiscountsController < ApplicationController
   def index
-    @merchant = Merchant.find(params[:merchant_id])
+    @merchant = find_merchant
   end
 
   def show
-
+    @merchant = find_merchant
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
 
   def new
-    @merchant = Merchant.find(params[:merchant_id])
+    @merchant = find_merchant
+  end
+
+  def update
+    @merchant = find_merchant
+    @bulk_discount = @merchant.bulk_discounts.find(params[:id])
+    @bulk_discount.update(bulk_params)
+    @bulk_discount.save
+    redirect_to "/merchant/#{@merchant.id}/bulk_discounts/#{@bulk_discount.id}"
+  end
+
+  def edit
+    @merchant = find_merchant
+    @bulk_discount = @merchant.bulk_discounts.find(params[:id])
   end
 
   def create
-    @merchant = Merchant.find(params[:merchant_id])
+    @merchant = find_merchant
     @merchant.bulk_discounts.create!(percent: params[:percent], threshold: params[:threshold])
     redirect_to "/merchant/#{@merchant.id}/bulk_discounts"
   end
 
   def destroy
-    merchant = Merchant.find(params[:merchant_id])
+    merchant = find_merchant
     bulk_discount = BulkDiscount.find(params[:id])
     bulk_discount.destroy
     redirect_to "/merchant/#{merchant.id}/bulk_discounts"
+  end
+
+  private
+  def find_merchant
+    Merchant.find(params[:merchant_id])
+  end
+
+  def bulk_params
+    params.permit(:percent, :threshold)
   end
 end
